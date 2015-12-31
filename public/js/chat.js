ab650bbd9623a016e9c42badf659088d9d5ac371
brandonfujii <brandonfujii2018@u.northwestern.edu>
2015-12-30 17:10:31 -0800

    added css dependencies

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,6 +8,7 @@ $form.submit(function(e){
 	socket.emit('chat message', $input.val());
 	$input.val('');
 });
+
 socket.on('chat message', function(msg){
 	$messages.append($('<li>').text(msg));
 });
@@ -15,6 +16,7 @@ socket.on('chat message', function(msg){
 socket.on('connect', function(msg) {
 	$messages.append($('<li>').text("A user has connected"));
 });
+
 socket.on('disconnect', function(msg) {
 	$messages.append($('<li>').text("A user has disconnected"));
 })
