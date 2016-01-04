83a56ad52bf89d7ba8dadca07d27d7f5155d325b
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-01-04 15:31:42 -0600

    added comments for server and client js

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,11 +21,17 @@ $menuIcon.on("click", function() {
 	$(this).toggleClass("active");
 });
 
+//disable chat and prompt user for username 
+//when connecting
 function promptLogin() {
 	$wrapper.show();
 	$userForm.hide();
 }
 
+//submit form to add a user
+	//if : data exists, prompt for a username
+	//else : send error message 
+	
 $userForm.submit(function(e) {
 	e.preventDefault();
 	socket.emit('enter user', $userName.val(), function(data) {
@@ -35,37 +41,47 @@ $userForm.submit(function(e) {
 			$userName.attr("placeholder", "Display name taken. Try again.");
 		}
 	});
+	//empty form field regardless after input
 	$userName.val('');
 });
 
+// print all users from user object to $userList
 socket.on("usernames", function(data) {
 	$userList.html('');
 	var users = '';
 	var len = data.length;
+	//loop through data to append list w/ username to users variable
 	for ( var i = 0; i < len; i++ ) {
 		users += '<li class="user">' + data[i] + '</li>';
 	} 
-	
+	//refresh userList with updated users variable
 	$userList.html(users);
 });
 
+// form field for sending a message
 $form.submit(function(e){
+	// prevent actual submission of form
 	e.preventDefault();
+	// if : form field is populated send msg with input's value
 	if ($input.val()) {
 		socket.emit('message', $input.val());
 	}
+	// empty form field regardless after input
 	$input.val('');
 });
 
+// appends user's message to $messages list
 socket.on('send message', function(msg){
 	$messages.append($('<li>').html("<span>" + msg.user + "</span>: " + msg.text));
 });
 
+// display a notification message when user connects
 socket.on('connect', function(data) {
 	$messages.append($('<li class="user-status connect">').text("A user has connected"));
 
 });
 
+// display a notification message when user disconnects
 socket.on('disconnect', function(msg) {
 	$messages.append($('<li class="user-status disconnect">').text("A user has disconnected"));
 	promptLogin();
